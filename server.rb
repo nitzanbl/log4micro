7090ee325834743acdb2fb570e48e7a00bbc7478
nitzanbl <nitzanblanko@gmail.com>
2017-03-19 13:20:48 +0200

    added route

--- server.rb
+++ server.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'pg'
+require 'json'
+
+set :db, PG.connect(host: 'localhost', user: 'postgres', dbname: 'log4micro', password: 'log4micro')
+configure do
+  mime_type :json, 'application/json'
+end
 
 get '/home' do
   'hello'
+end
+
+get '/projects' do
+  content_type :json
+  projects = []
+  settings.db.exec('select * from projects;') do |res|
+    res.each do |row|
+      projects << row
+    end
+  end
+  JSON.generate(projects)
 end
